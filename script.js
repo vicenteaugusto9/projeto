920fbb38553d759566ef997f14dd5f5afd8dd55d
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2024-09-15 22:38:44 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    // pegar tag img
+    const img = document.querySelector('#profile img')
+    // substituir a img
+    if(html.classList.contains('light')) {
+        // si tiver ligth.mode, adicionar a imagem light 
+       img.setAttribute('src','assets/avatar-light.png ')
+    } else {
+    //   se tiver dark,mode adicionar a img dark,mode
+     img.setAttribute('src','assets/avatar.png ')
+    }
+
+  
+}
